ff91f8de2be521af04e4d8700e8f130f4333c223
Jeadalyn Adique <jeadaadique@yahoo.com>
2016-03-09 15:38:07 +0800

    Assignment in Electives 3 using VB.net language

---
+++ HelloWorldElecs3/HelloWorldElecs3/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(104, 181)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 0
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnOK)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello World"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+
+End Class
---
+++ HelloWorldElecs3/HelloWorldElecs3/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub btnOK_Click(sender As System.Object, e As System.EventArgs) Handles btnOK.Click
+        MessageBox.Show("Hello World!", "Hello World!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Application.Exit()
+    End Sub
+End Class
